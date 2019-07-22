93a5317c0fad1d0db9c70e56dbfefe3e58653a8c
bklastai <benas@acmeaom.com>
2019-07-22 12:20:02 -0700

    forgot to hook up photo viewing activity

--- app/src/main/java/com/bklastai/imgurbrowser/activities/ImageViewingActivity.kt
+++ app/src/main/java/com/bklastai/imgurbrowser/activities/ImageViewingActivity.kt
@@ -3,11 +3,22 @@ package com.bklastai.imgurbrowser.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.bklastai.imgurbrowser.R
+import com.bklastai.imgurbrowser.views.IMAGE_LINK
+import com.bklastai.imgurbrowser.views.IMAGE_TITLE
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_image_viewing.*
+import kotlinx.android.synthetic.main.search_result_item.view.*
 
 class ImageViewingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image_viewing)
+
+        Picasso.get().load(intent.getStringExtra(IMAGE_LINK) ?: "").into(image)
+        image_title.text = intent.getStringExtra(IMAGE_TITLE) ?: ""
+        back_arrow.setOnClickListener {
+            finish()
+        }
     }
 }
--- app/src/main/java/com/bklastai/imgurbrowser/views/SearchResultViewHolder.kt
+++ app/src/main/java/com/bklastai/imgurbrowser/views/SearchResultViewHolder.kt
@@ -1,13 +1,18 @@
 package com.bklastai.imgurbrowser.views
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bklastai.imgurbrowser.R
+import com.bklastai.imgurbrowser.activities.ImageViewingActivity
 import com.bklastai.imgurbrowser.networking.SearchResult
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.search_result_item.view.*
+
+const val IMAGE_LINK  = "com.bklastai.imgurbrowser.views.IMAGE_LINK"
+const val IMAGE_TITLE  = "com.bklastai.imgurbrowser.views.IMAGE_TITLE"
 
 class SearchResultViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
@@ -15,6 +20,13 @@ class SearchResultViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         if (searchResult != null) {
             itemView.search_result_item_title.text = searchResult.title
             Picasso.get().load(searchResult.image).into(itemView.search_result_item_image)
+            itemView.search_result_item_image.setOnClickListener {
+                val intent = Intent(itemView.search_result_item_image.context, ImageViewingActivity::class.java).apply {
+                    putExtra(IMAGE_TITLE, searchResult.title)
+                    putExtra(IMAGE_LINK, searchResult.image)
+                }
+                itemView.search_result_item_image.context.startActivity(intent)
+            }
         }
     }
 
